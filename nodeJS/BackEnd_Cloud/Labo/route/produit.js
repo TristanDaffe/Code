1ad2e20c4650c76d242a67b47f9b27ad28742d10
etu45639@henallux.be <compteHENA0>
2022-10-05 14:42:30 +0200

    back end & cloud

--- nodeJS/BackEnd_Cloud/Labo/route/produit.js
+++ nodeJS/BackEnd_Cloud/Labo/route/produit.js
@@ -1,5 +1,8 @@
-const ProduitControleur = require("../controleur/produit");
-const router = require("express").Router();
+//const ProduitControleur = require("../controleur/produit");
+const ProduitControleur = require("../controleur/produitDB");
+//const router = require("express").Router();
+const Router = require("express-promise-router");
+const router = new Router;
 
 router.get('/:id', ProduitControleur.getProduit);
 router.post('/', ProduitControleur.postProduit);
